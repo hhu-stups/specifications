7f1d5528cf3585311fe379244738f7b78636e3b7
Michael Leuschel <leuschel@uni-duesseldorf.de>
2020-05-28 12:46:37 +0200

    improve CSV export, include root filename

--- scripts/db_tools.pl
+++ scripts/db_tools.pl
@@ -30,6 +30,7 @@ attribute(static_assertions).
 attribute(variables).
 
 codes_list_attr(file).
+codes_list_attr(name).
 codes_list_attr(sha256).
 
 last_attr(variables).
@@ -37,9 +38,10 @@ last_attr(variables).
 print_csv :- print_csv_stream(user_output).
 print_csv(File) :- open(File,write,S), print_csv_stream(S), close(S).
 
-print_csv_stream(S) :- format(S,'Nr,',[]), attribute(Attr), pr_attr(S,Attr),fail.
-print_csv_stream(S) :- file(Nr,_Path),
-     pr_attr(S,id,Nr),
+print_csv_stream(S) :- format(S,'Nr,Name,',[]), attribute(Attr), pr_attr(S,Attr),fail.
+print_csv_stream(S) :- file(Nr,Path),
+     pr_attr(S,id,Nr), get_tail_filename(Path,Name),
+     pr_attr(S,name,Name),
      attribute(Attr),
      (call(Attr,Nr,Val) -> pr_attr(S,Attr,Val) ; pr_attr(S,Attr,'')),
      fail.
@@ -52,3 +54,19 @@ pr_attr(S,Attr,Val) :- format(S,'~w',[Val]),pr_comma(S,Attr).
 
 pr_comma(S,Attr) :- last_attr(Attr),!,nl(S).
 pr_comma(S,_Attr) :- write(S,',').
+
+:- use_module(library(lists)).
+get_tail_filename(Path,Tail) :- 
+   (split_last_lst(Path, "/\\",  T) -> Tail=T ; Tail=Path).
+split_last_lst(ListAscii, Seps, TailA) :-
+   split_last2_lst(ListAscii,Seps,[],[],_HeadA, TailA).
+
+split_last2_lst([],_,CurSplit,[_|Head],ResH,ResT) :-
+   reverse(CurSplit,ResT),
+   reverse(Head,ResH).
+split_last2_lst([Sep|Tail],Seps,CurSplit,Head,ResH,ResT) :- member(Sep,Seps), % TO DO: use ord_member ?
+   !,
+   append([Sep|CurSplit],Head,NewHead),
+   split_last2_lst(Tail,Seps,[],NewHead,ResH,ResT).
+split_last2_lst([H|Tail],Seps,CurSplit,Head,ResH,ResT) :-
+   split_last2_lst(Tail,Seps,[H|CurSplit],Head,ResH,ResT).
